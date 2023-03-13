a5fc024b97966988040c2062e41c886e6d45362c
zy <millerevan@163.com>
2023-03-13 11:51:39 +0800

    os and shutil

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PythonPractice
 Some practice for Python study
+1.os and shutil
---
+++ os_and_shutil.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+def search_dir(dir,result):
+    child_files = os.listdir(dir)
+    for child_file in child_files:
+        child_file = os.path.join(dir,child_file)
+        result.append(child_file)
+        if os.path.isdir(child_file):
+            search_dir(child_file,result)
+
+result = []
+search_dir("E:\My Origninal Character",result)
+output = "E:\My Origninal Character\picture"
+os.makedirs(output)
+for file in result:
+    if os.path.isfile(file) and (file.endswith("jpg") or file.endswith("png")):
+        shutil.move(file,output)
