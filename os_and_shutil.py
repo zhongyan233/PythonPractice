8ae36d64237c77c9fc0afac741b0abec8267221f
zy <millerevan@163.com>
2023-03-24 13:44:18 +0800

    修正逻辑错误

--- os_and_shutil.py
+++ os_and_shutil.py
@@ -2,15 +2,17 @@ import os
 import shutil
 
 def search_dir(dir,result):
-    child_files = os.listdir(dir)
+    child_files = os.listdir(dir)# 
     for child_file in child_files:
         child_file = os.path.join(dir,child_file)
-        result.append(child_file)
         if os.path.isdir(child_file):
             search_dir(child_file,result)
+        else:
+            result.append(child_file)
 
 result = []
 search_dir("E:\My Origninal Character",result)
+# print(result)
 output = "E:\My Origninal Character\picture"
 os.makedirs(output)
 for file in result:
